c0d108a7791de8452dc968e50426cb83f2f2c898
Patricia Lindner <lindnerp@ohio.edu>
2023-09-25 16:40:29 -0400

    Adding the Big 3 to DArray class.

--- DynamicMemory/DArray.cc
+++ DynamicMemory/DArray.cc
@@ -8,15 +8,52 @@ DArray::DArray(){
     data = new int[size];
 }
 
+DArray::~DArray(){
+    delete []data;
+}
+
+DArray::DArray(const DArray& other){
+    size = other.size;
+    used = other.used;
+
+    data = new int[size];
+    // copy stuff using a loop or the copy function from algorithm
+}
+
+void DArray::operator = (const DArray& other){
+    // check for self assignment
+    if(this == &other){
+        return;
+    }
+
+    // deallocate the existing array
+    // put the deconstructor code here
+
+    // make a copy
+    // put the copy constructor code here
+}
+
 void DArray::resize(){
+    // Create a tmp pointer and allocate a bigger array
     int *tmp = new int[size + 5];
 
+    // Copy all items to the new array
     for(int i = 0; i < used; i++){
         tmp[i] = data[i];
     }
+    
+    // Instead of the for loop above, you can use the copy function from the algorithm library
+    // #include <algorithm> // this should go at the beginning of the file
+    // copy(begin, end, destination)
+    // copy(data, data + used, tmp);
 
+    // Deallocate the old array
     delete [] data;
+
+    // Move the data pointer to the new array
     data = tmp;
+
+    // Remember that you increased the size by 5
     size += 5;
 }
 
--- DynamicMemory/DArray.h
+++ DynamicMemory/DArray.h
@@ -1,11 +1,46 @@
+
 // macro guards
 #ifndef DARRAY_H
 #define DARRAY_H
 
 class DArray{
     public:
+        // The Big 3
+
+        // Destructor or Deconstructor
+        /**
+         * @brief Destroy the DArray object (deallocate any dynamically allocated memory)
+         * 
+         */
+        ~DArray();
+
+        // Copy constructor
+        /**
+         * @brief Construct a new DArray object that is a copy of an existing one
+         * 
+         * @param other - the object you want to make a copy of
+         */
+        DArray(const DArray& other);
+
+        // overloaded assignment operator
+        /**
+         * @brief Make this object a copy of another one
+         * 
+         * @param other - the object you want to make a copy of
+         */
+        void operator = (const DArray& other);
+
+        /**
+         * @brief Construct a new DArray object
+         * 
+         */
         DArray();
 
+        /**
+         * @brief Add a new item into the array
+         * 
+         * @param item - the item you want to add
+         */
         void add_item(int item);
 
     private:

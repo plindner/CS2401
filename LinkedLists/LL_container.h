ab86676cdb9461eb41d551622be0f24a097b0fb0
Patricia Lindner <lindnerp@ohio.edu>
2023-10-11 12:29:18 -0400

    Adding linked list container code (without and with tail)

---
+++ LinkedLists/LL_container.h
@@ -0,0 +1,33 @@
+class Node{
+    public:
+        Node(int d = 0, Node* n = nullptr);
+
+        int data()const {return data_field;}
+        Node* next()const {return next_field;}
+
+        void set_data(int d) {data_field = d;}
+        void set_next(Node* n) {next_field = n;}
+
+    private:
+        int data_field;
+        Node* next_field;
+};
+
+class LL_Container{
+    public:
+        LL_Container() {head = nullptr;}
+
+        // The Big 3
+        ~LL_Container();
+        LL_Container(const LL_Container& other);
+        LL_Container& operator = (const LL_Container& other);
+
+        // other useful stuff you might want to do
+        void add_head(int item);
+        void add_end(int item);
+        void remove(int target);
+
+        void display()const;
+    private:
+        Node* head;
+};

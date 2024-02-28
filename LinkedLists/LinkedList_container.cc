0ba79220eef2482260505f1867bceca33efb5b23
Patricia Lindner <lindnerp@ohio.edu>
2024-02-28 13:09:06 -0500

    Adding linked list container with tail code.

--- LinkedLists/LinkedList_container.cc
+++ LinkedLists/LinkedList_container.cc
@@ -1,4 +1,7 @@
-#include "LinkedList_container.h"
+/**
+*   Implementation file for a linked list container with a tail pointer.
+*   Code that is commented out only works where there is NOT a tail pointer.
+*/
 
 Node::Node(int d, Node* n){
     data_field = d;
@@ -11,7 +14,7 @@ LL_Container::~LL_Container(){
 
     while(head != nullptr){
         cursor = head;
-        head = head -> next();
+        head = head -> next;
         delete cursor;
     }
 }
@@ -22,20 +25,21 @@ LL_Container::LL_Container(const LL_Container& other){
         return;
     }
     else{
-        Node *src, *dest;
+        Node *src;
+        // If there is no tail in the container, add a dest Node* and replace tail
         head = new Node(other.head -> data());
+        tail = head;
 
         src = other.head -> next();
-        dest = head;
         while(src != nullptr){
-            dest -> set_next(new Node(src -> data()));
+            tail -> set_next(new Node(src -> data()));
             src = src -> next();
-            dest = dest -> next();
+            tail = tail -> next();
         }
     }
 }
 
-LL_Container& LL_Container::operator = (const LL_Container& other){
+LL_Container& operator = (const LL_Container& other){
     // check for self-assignment
 
     // destroy the old list
@@ -43,4 +47,83 @@ LL_Container& LL_Container::operator = (const LL_Container& other){
     // copy the other list
 
     return *this;
+}
+
+
+void LL_Container::display(ostream& outs) const{
+    Node* cursor = head;
+
+    while(cursor != nullptr){
+        outs << cursor -> data() << endl;
+        cursor = cursor -> next();
+    }
+}
+
+
+void LL_Container::add_to_end(int item){
+    // Node* cursor = head;
+
+    // if(head == nullptr){
+    //     head = new Node(item);
+    //     return;
+    // }
+
+    // for(; cursor -> next() != nullptr; cursor = cursor -> next());
+
+    // cursor -> set_next(new Node);
+    // cursor = cursor -> next();
+    // cursor -> set_data(item);
+
+    // tail = cursor;
+
+    if(tail == nullptr){
+        head = tail = new Node(item);
+        return;
+    }
+    tail -> set_next(new Node(item));
+    tail = tail -> next();
+
+}
+
+void LL_Container::add_to_head(int item){
+    head = new Node(item, head);
+
+    // Only need if you have a tail pointer
+    if(tail == nullptr) tail = head;
+}
+
+void LL_Container::remove(int target){
+    // empty list
+    if(head == nullptr) return;
+
+    // move cursor to the node being removed
+    // move prev to the node before the one being removed
+    Node* cursor = head;
+    Node* prev = head;
+    while(cursor != nullptr && cursor -> data() != target){
+        prev = cursor;
+        cursor = cursor -> next();
+    }
+
+    // case for found the node and list has more than one node or we are not removing the first node
+    if(cursor != nullptr && cursor != prev){
+        prev -> set_next(cursor -> next());
+        if(cursor == tail) tail = prev;
+        delete cursor;
+    }
+    // case for removing head of long list
+    // only works for linked list with tail.
+    // if you don't have a tail, you don't need this case
+    else if(cursor == head && tail != head){
+        head = cursor -> next();
+        delete cursor;
+    }
+    // case for removing last node of a single node list
+    // also works to remove the head node of a longer list when you don't have a tail if you remove the line that resets tail
+    else if(cursor != nullptr){
+        head = prev -> next();
+        // only need if you have a tail pointer
+        tail = nullptr;
+        delete prev;
+    }
 }
--- LinkedLists/LinkedList_container.h
+++ LinkedLists/LinkedList_container.h
@@ -1,85 +1,40 @@
-/**
- * @brief This file defines a Node class and a Linked List container class.
- *        The Node will be used to create the elements of the list dynamically.
- * 
- */
-
 class Node{
     public:
-        /**
-         * @brief Construct a new Node object with data set to 0 and next set to nullptr by default
-         * 
-         * @param d - optional argument to set the data upon creation
-         * @param n - optional argument to set the next pointer upon creation
-         */
-        Node(int d = 0, Node* n = nullptr);
+        typedef int value_type;
 
-        // accessor functions
-        /**
-         * @brief return the data value stored in this node
-         * 
-         * @return int - the data
-         */
-        int data() const {return data_field;}
+        Node(value_type d = 0, Node* n = nullptr);
 
-        /**
-         * @brief return the value of the next pointer in this node
-         * 
-         * @return Node* - the pointer to the next node
-         */
+        // accessor function
+        value_type data() const {return data_field;}
         Node* next() const {return next_field;}
 
         // mutators
-        /**
-         * @brief Set the data member
-         * 
-         * @param d - the value to store in the node
-         */
-        void set_data(int d) {data_field = d;}
-
-        /**
-         * @brief Set the next member
-         * 
-         * @param n - the location of the next node in the list
-         */
+        void set_data(value_type d) {data_field = d;}
         void set_next(Node* n) {next_field = n;}
 
     private:
-        int data_field;
+        value_type data_field;
         Node* next_field;
 };
 
 
 class LL_Container{
     public:
-        /**
-         * @brief Construct a new LL_Container object with an empty list
-         * 
-         */
-        LL_Container() {head = nullptr;}
+        LL_Container() {head = tail = nullptr;}
 
         // The Big 3
-        /**
-         * @brief Destroy the LL_Container object deleting all nodes
-         * 
-         */
         ~LL_Container();
-
-        /**
-         * @brief Construct a new LL_Container object that is a copy of another
-         * 
-         * @param other - the object you want this one to be a copy of
-         */
         LL_Container(const LL_Container& other);
-
-        /**
-         * @brief Overloaded assignment operator for LL_Containers
-         * 
-         * @param other - the object you want this one to be a copy of
-         * @return LL_Container& - a copy of this object so you can daisy-chain the operator
-         */
         LL_Container& operator = (const LL_Container& other);
+
+        // Other useful things
+        void display(ostream& outs) const;
+
+        void add_to_end(int item);
+        void remove(int target);
+        void add_to_head(int item);
 
     private:
         Node* head;
+        Node* tail;
 };
